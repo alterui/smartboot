b6c79d4e3411a8869ec879409b85625f9894d45f
liurui <liu.rui@oyohotels.cn>
2019-08-28 14:22:55 +0800

    copy file

--- src/main/java/com/oyo/system/CopyFile.java
+++ src/main/java/com/oyo/system/CopyFile.java
@@ -1,30 +1,37 @@
 package com.oyo.system;
 
 
+import sun.reflect.misc.FieldUtil;
+
 import java.io.*;
 import java.nio.channels.FileChannel;
-
 /**
  * @author liurui
  * @date 2019/8/28 10:28
  */
 public class CopyFile {
     public static void main(String[] args) {
-        File sourceFile = new File("C:\\myData\\lock\\client.exe");
-        File destFile = new File("C:\\lock4\\client.exe");
+     /*   File sourceFile = new File("C:\\myData\\lock\\client.exe");
+        File destFile = new File("C:\\lock4\\client.exe");*/
+
+        File sourceFile = new File("/Users/oyo/Documents/book/Java8实战.pdf");
+        File destFile = new File("/Users/oyo/Documents/book1/Java8实战.pdf");
+
+
 
         long beforeTime = System.currentTimeMillis();
 
         copyFile(sourceFile, destFile);
         System.out.println(System.currentTimeMillis()-beforeTime);
 
+
     }
 
-    public static void copyFile(File source, File dest) {
-        if (!dest.exists()) {
-            dest.getParentFile().mkdir();
+    public static void copyFile(File source, File target) {
+        if (!target.exists()) {
+            target.getParentFile().mkdir();
             try {
-                dest.createNewFile();
+                target.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -33,11 +40,8 @@ public class CopyFile {
         FileChannel outputChannel = null;
         try {
             inputChannel = new FileInputStream(source).getChannel();
-            outputChannel = new FileOutputStream(dest).getChannel();
-
-
+            outputChannel = new FileOutputStream(target).getChannel();
             outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
