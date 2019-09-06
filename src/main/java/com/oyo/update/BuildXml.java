88b37376b46fb7043e1b2bb0c11780a2484aa3b8
liurui <liu.rui@oyohotels.cn>
2019-09-06 10:49:48 +0800

    add version.

--- src/main/java/com/oyo/update/BuildXml.java
+++ src/main/java/com/oyo/update/BuildXml.java
@@ -2,16 +2,11 @@ package com.oyo.update;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.AttributesImpl;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.sax.SAXTransformerFactory;
-import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.List;
@@ -21,7 +16,7 @@ import java.util.List;
  * @date 2019/9/5 20:07
  */
 public class BuildXml {
-    public static void createXMLByDOM(File dest) {
+    public static void createXMLByDOM(File source,File target) {
         // 创建DocumentBuilderFactory
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
@@ -34,13 +29,14 @@ public class BuildXml {
 
             // 设置XML声明中standalone为yes，即没有dtd和schema作为该XML的说明文档，且不显示该属性
             // document.setXmlStandalone(true);
-
             // 创建根节点
-            Element bookstore = document.createElement("files");
+            Element files = document.createElement("files");
 
-            List<String> fileAbsolutePath = FileTest.getFileAbsolutePath(new File("/Users/oyo/Downloads/picture"));
+            Element fileVersion = document.createElement("version");
+            fileVersion.setTextContent(source.getName());
+            files.appendChild(fileVersion);
 
-
+            List<String> fileAbsolutePath = FileTest.getFileAbsolutePath(source);
             for (int i = 0; i < fileAbsolutePath.size(); i++) {
                 // 创建子节点，并设置属性
                 Element  file = document.createElement("file");
@@ -61,23 +57,15 @@ public class BuildXml {
                 fileMD5Code.setTextContent(FileUtils.getFileMD5Code(new File(fileAbsolutePath.get(i))));
                 file.appendChild(fileMD5Code);
 
-
                 // 为根节点添加子节点
-                bookstore.appendChild(file);
-
-
+                files.appendChild(file);
             }
 
-
-
-
-
-
             // 将根节点添加到Document下
-            document.appendChild(bookstore);
+            document.appendChild(files);
 
-            /*
-             * 下面开始实现： 生成XML文件
+            /**
+             * 生成XML文件
              */
 
             // 创建TransformerFactory对象
@@ -90,22 +78,19 @@ public class BuildXml {
             tf.setOutputProperty(OutputKeys.INDENT, "yes");
 
             // 使用Transformer的transform()方法将DOM树转换成XML
-            tf.transform(new DOMSource(document), new StreamResult(dest));
+            tf.transform(new DOMSource(document), new StreamResult(target));
 
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (TransformerConfigurationException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (TransformerException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
 
     }
 
     public static void main(String[] args) {
-        createXMLByDOM(new File("/Users/oyo/workspace/smartboot/src/main/java/com/oyo/update/ex.xml"));
+        createXMLByDOM(new File("/Users/oyo/Downloads/1.0.2"),new File("/Users/oyo/Downloads/1.0.2/file.xml"));
     }
 }
