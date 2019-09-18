a1b187bfb09f6b206cbb8af70cea4d6d01add41a
liurui <liu.rui@oyohotels.cn>
2019-09-18 17:09:09 +0800

    add README.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# smartboot
-a
+#### 设计模式博客地址为：https://juejin.im/post/5d7e51226fb9a06af05ce457
---
+++ src/main/java/com/oyo/pattern_23/pattern02/factory01/Animal.java
@@ -0,0 +1,12 @@
+package com.oyo.pattern_23.pattern02.factory01;
+
+/**
+ * 描述要养的宠物
+ *
+ * @author liurui
+ * @date 2019/9/18 17:01
+ */
+public interface Animal {
+    void getAnimal();
+}
+
